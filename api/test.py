38a0c160acae545d283f4768e4c791512649fc9d
KrGennadiy <al.kuv.x@gmail.com>
2025-07-06 02:52:58 +0500

    work version without docker

--- api/test.py
+++ api/test.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, ValidationError
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -7,35 +9,67 @@ import uvicorn
 
 app = FastAPI()
 
-engine = create_engine("postgresql://root:root@localhost:5432/testdb")
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+engine = create_engine("postgresql://postgres:root@localhost:5432/tododb")
 base = declarative_base()
 
 
-class TestDB(base):
-    __tablename__ = "test"
+class TaskDB(base):
+    __tablename__ = "tasks"
     id = Column(Integer, primary_key=True)
     text = Column(String)
 
 
 Session = sessionmaker(bind=engine)
 session = Session()
-print("создал сессию")
 
 
 class Task(BaseModel):
     text: str
+    id: int | None = None
 
 
-@app.post("/postTest/")
-async def create_task(test: Task):
+class PushTask(BaseModel):
+    text: str
+
+class DeleteTask(BaseModel):
+    id: int
+
+@app.post("/pullTask")
+async def toFrontTasks():
+    answer = session.query(TaskDB).all()
+    return answer
+
+
+@app.post("/pushTask")
+async def createTask(task: PushTask):
     try:
-        taskdb = TestDB(text=test.text)
+        taskdb = TaskDB(text=task.text)
         session.add(taskdb)
+        session.commit()
+        return {"message": taskdb}
+    except ValidationError as e:
+        return {"error": e.errors()}
+
+
+@app.post("/deleteTask")
+async def delete_task(task: DeleteTask):
+    try:
+        taskdb = TaskDB(id=task.id)
+        session.query(TaskDB).filter(TaskDB.id == task.id).delete()
         session.commit()
         return {"message": taskdb}
     except Exception as e:
         return {"error": str(e)}
 
 
-#if __name__ == "__main__":
-#    uvicorn.run(app, host="localhost", port=8088)
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8088)
--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,4 @@
 <template>
-
-
-
 	<table class="table">
 		<tr>
 			<td colspan="2">
@@ -10,76 +7,58 @@
 		</tr>
 		<tr v-for="(task, index) in tasks">
 			<td>{{ task.text }}</td>
-			<td><button @click="deleteTask(index)"> удалить </button> </td>
+			<td><button @click="deleteTask(task.id)"> удалить </button> </td>
 		</tr>
 		<tr>
 			<td><input type="text" v-model.trim="textTask" placeholder="задача"
-					@keyup.enter="clickButton('mainButton', textTask)">
+					@keyup.enter="pushToBack(), clearInput()">
 			</td>
-			<td><button id="mainButton" v-show="textTask != ''" @click="addToArray(),
+			<td><button id="mainButton" v-show="textTask != ''" @click="pushToBack(),
 				clearInput()">Добавить</button>
 			</td>
 		</tr>
 	</table>
-
-
 </template>
 
 <script>
 import axios from 'axios'
+const api = axios.create({
+	baseURL: 'http://localhost:8088'
+})
+
 
 export default {
 	data() {
 		return {
-			mainHeader: 'to-do list in docker',
+			mainHeader: 'to-do list',
 			tasks: [],
 			textTask: ''
 		}
 	},
+	mounted() {
+		this.pullFromBack();
+		setInterval(this.pullFromBack, 500);
+	},
 	methods: {
-		//добавляю в массив tasks в переменную textTask (все работает в реактивном режиме)
-		addToArray() {
-			this.tasks.push({
-				text: this.textTask
-
-			})
-			axios.post('http://localhost:8088/postTest',
-				{
-					data: {
-						text: this.textTask,
-					}
-				},
-				{
-					responseType: 'json',
-					headers: {
-						'Content-Type': 'application/json'
-
-					}
-				}
-
-			)
-				.then(response => console.log(response.data))
+		pullFromBack() {
+			api.post('/pullTask')
+				.then(response => { this.tasks = response.data }, console.log(this.tasks))
+				.catch(error => console.error(error))
+		},
+		pushToBack() {
+			api.post('/pushTask', { text: this.textTask })
+				.then(response => console.log(response.text))
 				.catch(error => console.error(error))
 
 		},
-		//очистить поле ввода новой задачи
 		clearInput() {
 			this.textTask = ''
 		},
-		//удалить задачу из списка (index - номер задачи, с которой функция будет удалять элемент массива, 1 - кол-во удаленных элементов)
 		deleteTask(index) {
-			this.tasks.splice(index, 1)
-			axios.delete('http://localhost:8088/deleteTest',
-				{
-					id: index
-				}
+			api.post('/deleteTask', { id: index }, {_method: 'delete'}
 			)
 				.then(response => console.log(response.data))
 				.catch(error => console.error(error))
-		},
-		//валидация при внесении задачи в массив
-		clickButton(text) {
-			if (text != '') { this.addToArray() }
 		}
 	}
 }
