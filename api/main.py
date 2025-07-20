f1292119e05555e9b40f57d3b74f6518792dbda8
KrGennadiy <al.kuv.x@gmail.com>
2025-07-21 00:10:55 +0500

    небольшие правки

--- api/main.py
+++ api/main.py
@@ -1,10 +1,7 @@
-# import sys
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routers.tasks import router_tasks
-
-# sys.path.append('C:/dev/todolist')
 
 app = FastAPI()
 
@@ -22,6 +19,6 @@ app.include_router(router_tasks)
 app.get('/')
 async def root():
     return {"message": "root is work"}
-
+# обязательно убрать при сборке образа докер
 if __name__ == "__main__":
     uvicorn.run('main:app', host="localhost", port=8088, reload=True)
